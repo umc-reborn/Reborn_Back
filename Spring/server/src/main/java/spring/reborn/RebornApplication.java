789e2e7d730d958f0421ebcd4044f20f945d0e50
haochaen73 <kyjjulia07@gmail.com>
2023-01-11 15:58:15 +0900

    230111 project 생성
    java 11 spring boot 2.7.7

---
+++ Spring/server/src/main/java/spring/reborn/RebornApplication.java
@@ -0,0 +1,13 @@
+package spring.reborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RebornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RebornApplication.class, args);
+	}
+
+}
---
+++ Spring/server/src/main/java/spring/reborn/user/model/PostUserReq.java
@@ -0,0 +1,4 @@
+package spring.reborn.user.model;
+
+public class PostUserReq {
+}
