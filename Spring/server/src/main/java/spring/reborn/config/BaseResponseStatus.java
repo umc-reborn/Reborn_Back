15f23ec3ffb6bafbe60b21f6a19d67e64f2d5051
haochaen73 <kyjjulia07@gmail.com>
2023-01-11 15:58:15 +0900

    220111 project 생성
    java 11 spring boot 2.7.7

---
+++ Spring/server/src/main/java/spring/reborn/config/BaseResponseStatus.java
@@ -0,0 +1,67 @@
+package spring.reborn.config;
+
+import lombok.Getter;
+
+/**
+ * 에러 코드 관리
+ */
+@Getter
+public enum BaseResponseStatus {
+    /**
+     * 1000 : 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+
+
+    /**
+     * 2000 : Request 오류
+     */
+    // Common
+    REQUEST_ERROR(false, 2000, "입력값을 확인해주세요."),
+    EMPTY_JWT(false, 2001, "JWT를 입력해주세요."),
+    INVALID_JWT(false, 2002, "유효하지 않은 JWT입니다."),
+    INVALID_USER_JWT(false,2003,"권한이 없는 유저의 접근입니다."),
+
+    // users
+    USERS_EMPTY_USER_ID(false, 2010, "유저 아이디 값을 확인해주세요."),
+
+    // [POST] /users
+    POST_USERS_EMPTY_NAME(false, 2011, "유저 이름을 입력해주세요."),
+
+    /**
+     * 3000 : Response 오류
+     */
+    // Common
+    RESPONSE_ERROR(false, 3000, "값을 불러오는데 실패하였습니다."),
+
+    /**
+     * 4000 : Database, Server 오류
+     */
+    DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패하였습니다."),
+    SERVER_ERROR(false, 4001, "서버와의 연결에 실패하였습니다."),
+
+    //[PATCH] /users/{userIdx}
+    MODIFY_FAIL_USERNAME(false,4014,"유저네임 수정 실패"),
+    MODIFY_FAIL_PRODUCTNAME(false,4015,"상품네임 수정 실패"),
+    MODIFY_FAIL_PRICE(false,4016,"가격 수정 실패"),
+    MODIFY_FAIL_CATEGORY(false,4017,"카테고리 수정 실패"),
+    MODIFY_FAIL_STATUS(false,4018,"유저 비활성화 실패"),
+
+    PASSWORD_ENCRYPTION_ERROR(false, 4011, "비밀번호 암호화에 실패하였습니다."),
+    PASSWORD_DECRYPTION_ERROR(false, 4012, "비밀번호 복호화에 실패하였습니다.");
+
+
+    // 5000 : 필요시 만들어서 쓰세요
+    // 6000 : 필요시 만들어서 쓰세요
+
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) { //BaseResponseStatus 에서 각 해당하는 코드를 생성자로 맵핑
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
