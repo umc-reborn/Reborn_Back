37d776e4d5e2f3f8f4c45f8b8fee7c4e9ed921ff
Minjeong Kim <serak0310@naver.com>
2023-01-18 21:49:06 +0900

    [ADD] 유저 정보 조회

--- Spring/server/src/main/java/spring/reborn/domain/user/model/GetUserInformRes.java
+++ Spring/server/src/main/java/spring/reborn/domain/user/model/GetUserInformRes.java
@@ -3,7 +3,6 @@ package spring.reborn.domain.user.model;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 
@@ -11,10 +10,9 @@ import javax.persistence.Enumerated;
 @Setter
 @AllArgsConstructor
 public class GetUserInformRes {
-    private int userIdx;
+    private String userImg;
     private String userNickname;
     private String userAddress;
     private String userBirthDate;
-    @Enumerated(EnumType.STRING)
     private String userLikes;
 }
