e38a89c520c6e306e1900852a4a32a86ab79d05e
merong245 <merong245@gmail.com>
2023-01-23 13:34:23 +0900

    feat : 스토어 정보 수정 with 이미지 업로드 (#41)

--- Spring/server/src/main/java/spring/reborn/domain/store/StoreController.java
+++ Spring/server/src/main/java/spring/reborn/domain/store/StoreController.java
@@ -2,18 +2,21 @@ package spring.reborn.domain.store;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import spring.reborn.config.BaseException;
 import spring.reborn.config.BaseResponse;
 import spring.reborn.config.BaseResponseStatus;
+import spring.reborn.domain.awsS3.AwsS3Service;
 import spring.reborn.domain.store.model.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("/store")
 @RequiredArgsConstructor
-@Slf4j
 public class StoreController {
 
     private final StoreService storeService;
@@ -40,7 +43,7 @@ public class StoreController {
     가게 위치 표시
      */
     @GetMapping("/{storeIdx}/location")
-    public BaseResponse<GetStoreLocationRes> getStoreLocation(@PathVariable Long storeIdx) throws BaseException {
+    public BaseResponse<GetStoreLocationRes> getStoreLocation(@PathVariable Long storeIdx) {
 
         try {
             GetStoreLocationRes getStoreLocationRes = storeService.getStoreLocation(storeIdx);
@@ -57,7 +60,7 @@ public class StoreController {
     가게 정보 조회
      */
     @GetMapping("/{storeIdx}")
-    public BaseResponse<GetStoreRes> getStoreInfo(@PathVariable Long storeIdx) throws BaseException {
+    public BaseResponse<GetStoreRes> getStoreInfo(@PathVariable Long storeIdx) {
         try {
             GetStoreRes getStoreRes = storeService.getStoreInfo(storeIdx);
             return new BaseResponse<>(getStoreRes);
@@ -74,9 +77,9 @@ public class StoreController {
     가게 검색
      */
     @GetMapping("/search/{keyword}")
-    public BaseResponse<List<GetStoreRes>> searchStore(@PathVariable String keyword) throws BaseException {
+    public BaseResponse<List<GetStoreRes>> searchStore(@PathVariable String keyword) {
         try {
-            if(keyword.isEmpty())
+            if (keyword.isEmpty())
                 throw new BaseException(BaseResponseStatus.GET_STORE_EMPTY_KEYWORD);
 
             List<GetStoreRes> getStoreRes = storeService.searchStoreListUsingTitle(keyword);
@@ -92,15 +95,23 @@ public class StoreController {
     todo
     가게 정보 수정
      */
-    @PatchMapping("/{storeIdx}")
-    public BaseResponse<Object> updateStoreInfo(@PathVariable Long storeIdx, @RequestBody PatchStoreReq patchStoreReq) {
+    @PatchMapping(value = "/{storeIdx}",
+            consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE}
+    )
+    public BaseResponse<Object> updateStoreInfo(@PathVariable Long storeIdx,
+                                                @RequestPart PatchStoreReq patchStoreReq,
+                                                @RequestPart(name = "image",required = false) MultipartFile multipartFile) {
         try {
-            if(patchStoreReq.getStoreName().isEmpty())
+            if (patchStoreReq.getStoreName().isEmpty())
                 throw new BaseException(BaseResponseStatus.MODIFY_FAIL_STORE_EMPTY_NAME);
             if (patchStoreReq.getStoreAddress().isEmpty())
                 throw new BaseException(BaseResponseStatus.MODIFY_FAIL_STORE_EMPTY_LOCATION);
 
-            storeService.updateStoreInfo(storeIdx, patchStoreReq);
+            if(multipartFile == null)
+                storeService.updateStoreInfo(storeIdx, patchStoreReq);
+            else
+                storeService.updateStoreInfo(storeIdx, patchStoreReq,multipartFile);
+
             return new BaseResponse<>(new PatchStoreRes(storeIdx));
         } catch (BaseException e) {
             log.error(e.getStatus().getMessage());
