b0a219d012fe842eb2fdbbf94242004ae167473b
seunghb320 <seunghb320@gmail.com>
2023-01-23 14:56:40 +0900

    chore : 스토어 회원탈퇴, 회원가입 수정(리스폰스로 닉네임 추가 및 이미지 주입 반영 )

--- Spring/server/src/main/java/spring/reborn/domain/user/model/PostUserRes2.java
+++ Spring/server/src/main/java/spring/reborn/domain/user/model/PostUserRes2.java
@@ -7,9 +7,7 @@ import lombok.Setter;
 @Getter // 해당 클래스에 대한 접근자 생성
 @Setter // 해당 클래스에 대한 설정자 생성
 @AllArgsConstructor // 해당 클래스의 모든 멤버 변수(jwt, userIdx)를 받는 생성자를 생성-
-public class PostUserStoreRes {
-//    private int userIdx;
-    private int storeIdx;
-    //    해당 부분은 7주차 - JWT 수업 후 주석해제 및 대체해주세요!
-    private String jwt;
+public class PostUserRes2 {
+    private int userIdx;
+    private String userNickname;
 }
