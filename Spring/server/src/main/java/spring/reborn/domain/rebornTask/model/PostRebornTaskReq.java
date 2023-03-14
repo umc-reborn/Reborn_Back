ebe8231641173ef23fec18bff682d809a3fc4112
hyerimmy <heyrim2010@naver.com>
2023-03-15 02:12:16 +0900

    feat : redis 연결

--- Spring/server/src/main/java/spring/reborn/domain/rebornTask/model/PostRebornTaskReq.java
+++ Spring/server/src/main/java/spring/reborn/domain/rebornTask/model/PostRebornTaskReq.java
@@ -2,11 +2,13 @@ package spring.reborn.domain.rebornTask.model;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Builder
+//@Builder
+@NoArgsConstructor
 public class PostRebornTaskReq {
     private Long rebornIdx;
     private Long userIdx;
